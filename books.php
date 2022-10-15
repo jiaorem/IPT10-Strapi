54af7622cd78b146973c067ae220d4e805edd583
jiaorem <yabut.micohjomarie@auf.edu.ph>
2022-10-15 12:01:40 +0800

    first commit

---
+++ books.php
@@ -0,0 +1,167 @@
+<?php
+
+include "vendor/autoload.php";
+use GuzzleHttp\Client;
+
+function getBooks() {
+    $token = 'c643bd1bbe13eca61da7931184614d71e9e197c8d87bd86f2c7b435a717ab47e58ea2b707d4a226fc5349f521cafa07af3d019e8bd2c0ee61a4770e575aa0110241bd370e1a3894de3473138f498559c997e5c392fb4b797397c85a52f76f36a0542af63246865714be1eb62cc27ac09055c63c452aaa9d130d0d11b27132002';
+
+    $client = new Client([
+        'base_uri' => 'http://localhost:1337/api/',
+    ]);
+
+    $headers = [
+    'Authorization' => 'Bearer' . $token,
+    'Accept'        => 'application/json',
+    ];
+
+    $response = $client->request('GET', 'books', [
+        'headers' => $headers
+    ]);
+
+    $body = $response->getBody();
+    $contents = json_decode($body);
+    return $contents;
+}
+
+$books = getBooks();
+//var_dump($books);
+?>
+   
+
+   <!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>IPT10 STRAPI</title>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<style>
+body {
+	color: #566787;
+	background: #f5f5f5;
+	font-family: 'Varela Round', sans-serif;
+	font-size: 13px;
+}
+.table-responsive {
+    margin: 30px 0;
+}
+.table-wrapper {
+	background: #fff;
+	padding: 20px 25px;
+	border-radius: 3px;
+	min-width: 1000px;
+	box-shadow: 0 1px 1px rgba(0,0,0,.05);
+}
+.table-title {        
+	padding-bottom: 15px;
+	background: #435d7d;
+	color: #fff;
+	padding: 16px 30px;
+	min-width: 100%;
+	margin: -20px -25px 10px;
+	border-radius: 3px 3px 0 0;
+}
+.table-title h2 {
+	margin: 5px 0 0;
+	font-size: 24px;
+}
+.table-title .btn-group {
+	float: right;
+}
+.table-title .btn {
+	color: #fff;
+	float: right;
+	font-size: 13px;
+	border: none;
+	min-width: 50px;
+	border-radius: 2px;
+	border: none;
+	outline: none !important;
+	margin-left: 10px;
+}
+table.table tr th, table.table tr td {
+	border-color: #e9e9e9;
+	padding: 12px 15px;
+	vertical-align: middle;
+}
+table.table tr th:first-child {
+	width: 100px;
+}
+table.table tr th:last-child {
+	width: 100px;
+}
+table.table-striped tbody tr:nth-of-type(odd) {
+	background-color: #fcfcfc;
+}
+table.table-striped.table-hover tbody tr:hover {
+	background: #f5f5f5;
+}
+table.table .avatar {
+	border-radius: 50%;
+	vertical-align: middle;
+	margin-right: 10px;
+}
+.hint-text {
+	float: left;
+	margin-top: 10px;
+	font-size: 13px;
+}    
+</style>
+<script>
+$(document).ready(function(){
+	// Activate tooltip
+	$('[$class-toggle="tooltip"]').tooltip();
+});
+</script>
+</head>
+<body>
+
+<div class="container-xl">
+	<div class="table table-responsive">
+		<div class="table-wrapper">
+			<div class="table-title">
+				<div class="row">
+					<div class="col-sm-6">
+						<h2><b>Books</b></h2>
+					</div>
+				</div>
+			</div>
+			
+			<table class="table table-striped table-hover">
+				<thead>
+					<tr>
+						<th scope="col">ID</th>
+                        <th scope="col">Book Title</th>
+                        <th scope="col">Author</th>
+                        <th scope="col">Category</th>
+					</tr>
+				</thead>
+
+				<?php
+                $books = getBooks();
+                foreach($books->data as $book):
+                //$book = $bookData->attributes;
+                ?>
+				<tbody>
+            <tr>
+                <th scope="row"><?php echo $book->id ?></th>
+                <th scope="row"><?php echo $book->name ?></th>
+                <th scope="row"><?php echo $book->author ?></th>
+                <th scope="row"><?php echo $book->category ?></th>
+            </tr>
+				</tbody>
+
+				<?php endforeach; ?>
+			</table>
+		</div>
+	</div>        
+</div>
+</body>
+</html>
